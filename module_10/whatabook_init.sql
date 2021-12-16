5a28dae30961a73d90c1062bb8b4b59172f9b7ec
Chris <sewalson1975@gmail.com>
2021-12-15 22:15:27 -0600

    uncommented

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -5,65 +5,65 @@
     Description: whatabook database initialization script.
 */
 
--- -- drop test user if exists 
--- DROP USER IF EXISTS 'whatabook_user'@'localhost';
+-- drop test user if exists 
+DROP USER IF EXISTS 'whatabook_user'@'localhost';
 
 
--- -- create whatabook_user and grant them all privileges to the whatabook database 
--- CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
+-- create whatabook_user and grant them all privileges to the whatabook database 
+CREATE USER 'whatabook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL8IsGreat!';
 
--- -- grant all privileges to the whatabook database to user whatabook_user on localhost 
--- GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
+-- grant all privileges to the whatabook database to user whatabook_user on localhost 
+GRANT ALL PRIVILEGES ON whatabook.* TO'whatabook_user'@'localhost';
 
--- -- dropping constraints that may exist???
--- ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
--- ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
+-- dropping constraints that may exist???
+ALTER TABLE wishlist DROP FOREIGN KEY fk_book;
+ALTER TABLE wishlist DROP FOREIGN KEY fk_user;
 
--- -- dropping tables
--- DROP TABLE IF EXISTS store;
--- DROP TABLE IF EXISTS book;
--- DROP TABLE IF EXISTS wishlist;
--- DROP TABLE IF EXISTS user;
+-- dropping tables
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS user;
 
--- /*
--- Creating tables next
--- */
--- CREATE TABLE store(
---     store_id    INT             NOT NULL    AUTO_INCREMENT,
---     locale      VARCHAR(500)    NOT NULL,
---     PRIMARY KEY(store_id)
--- );
+/*
+Creating tables next
+*/
+CREATE TABLE store(
+    store_id    INT             NOT NULL    AUTO_INCREMENT,
+    locale      VARCHAR(500)    NOT NULL,
+    PRIMARY KEY(store_id)
+);
 
--- CREATE TABLE book(
---     book_id     INT             NOT NULL    AUTO_INCREMENT,
---     book_name   VARCHAR(200)    NOT NULL,
---     author      VARCHAR(200)    NOT NULL,
---     details     VARCHAR(500),
---     PRIMARY KEY(book_id)
--- );
+CREATE TABLE book(
+    book_id     INT             NOT NULL    AUTO_INCREMENT,
+    book_name   VARCHAR(200)    NOT NULL,
+    author      VARCHAR(200)    NOT NULL,
+    details     VARCHAR(500),
+    PRIMARY KEY(book_id)
+);
 
--- CREATE TABLE user(
---     user_id     INT             NOT NULL    AUTO_INCREMENT,
---     first_name  VARCHAR(75)     NOT NULL,
---     last_name   VARCHAR(75)     NOT NULL,
---     PRIMARY KEY(user_id)
--- );
+CREATE TABLE user(
+    user_id     INT             NOT NULL    AUTO_INCREMENT,
+    first_name  VARCHAR(75)     NOT NULL,
+    last_name   VARCHAR(75)     NOT NULL,
+    PRIMARY KEY(user_id)
+);
 
--- CREATE TABLE wishlist(
---     wishlist_id INT             NOT NULL    AUTO_INCREMENT,
---     user_id     INT             NOT NULL,
---     book_id     INT             NOT NULL,
---     PRIMARY KEY(wishlist_id),
---     CONSTRAINT fk_book
---     FOREIGN KEY(book_id)
---         REFERENCES book(book_id),
---     CONSTRAINT fk_user
---     FOREIGN KEY(user_id)
---         REFERENCES user(user_id)
--- );
--- /*
---     inserting store information 
--- */
+CREATE TABLE wishlist(
+    wishlist_id INT             NOT NULL    AUTO_INCREMENT,
+    user_id     INT             NOT NULL,
+    book_id     INT             NOT NULL,
+    PRIMARY KEY(wishlist_id),
+    CONSTRAINT fk_book
+    FOREIGN KEY(book_id)
+        REFERENCES book(book_id),
+    CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+        REFERENCES user(user_id)
+);
+/*
+    inserting store information 
+*/
 INSERT INTO store(locale)
     VALUES('4578 Helen St., Sioux City IA, 51106');
 
